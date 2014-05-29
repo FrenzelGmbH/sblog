ebf0b228b8f08fdae447d189bb9ccc27ef4bc9b7
Philipp Frenzel <philipp@frenzel.net>
2014-05-29 19:58:16 +0200

    fixing pagination links

--- widgets/views/_post_pagination.php
+++ widgets/views/_post_pagination.php
@@ -7,13 +7,15 @@ use yii\helpers\Html;
 <div class="clearfix"></div>
 
 <div class="btn btn-default prev-btn">
-  <?php 
-    echo Html::a('previous',['/posts/post/onlineview','id'=>$model->previousPost->id]);
+  <?php
+    if(is_object($model->previousPost))
+      echo Html::a('previous',['/posts/post/onlineview','id'=>$model->previousPost->id]);
   ?>
 </div>
 
 <div class="btn btn-default next-btn pull-right">
   <?php 
-    echo Html::a('next',['/posts/post/onlineview','id'=>$model->nextPost->id]);
+    if(is_object($model->nextPost))
+      echo Html::a('next',['/posts/post/onlineview','id'=>$model->nextPost->id]);
   ?>
 </div>
