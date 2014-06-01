dd1a2ad0a91133267d5e2b8b27ce3dae2ad6e253
Philipp Frenzel <philipp@frenzel.net>
2014-06-01 09:14:23 +0200

    fix column2 layout

--- views/layouts/column2.php
+++ views/layouts/column2.php
@@ -1,16 +1,7 @@
 <?php $this->beginContent('@adminUi/themes/layouts/main.php'); ?>
 <div id="content">
-
-  <div class="row">
-    <div class="col-md-4">      
-      <?= $this->blocks['sidebar']; ?>
-    </div>
-    <div class="col-md-8">
-      <div class="cms">
-        <?= $content; ?>
-      </div>
-    </div>
+  <div class="cms">
+    <?= $content; ?>
   </div>
-  
 </div><!-- container -->
 <?php $this->endContent(); ?>
